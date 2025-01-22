4493c9a43a5f9cb9445e16d1ce55ae9876007560
SarahNasir54 <sarah.nasir540@gmail.com>
2025-01-22 12:56:49 +0500

    item.py updated

--- src/keepup_scrappers/items.py
+++ src/keepup_scrappers/items.py
@@ -19,3 +19,20 @@ class IVerifyItem(scrapy.Item):
     label = scrapy.Field()
     content = scrapy.Field()
     author = scrapy.Field()
+
+class GFCItem(scrapy.Item):
+    title = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+    detail_url = scrapy.Field()
+    publication_date = scrapy.Field()
+    content = scrapy.Field()
+    author = scrapy.Field()
+
+class FridayTItem(scrapy.Item):
+    title = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+    detail_url = scrapy.Field()
+    publication_date = scrapy.Field()
+    content = scrapy.Field()
