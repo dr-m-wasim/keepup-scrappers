82342dfda23c853a1b41b94b545552eaeb854056
SarahNasir54 <sarah.nasir540@gmail.com>
2025-01-23 10:57:52 +0500

    added PolitifactItems in items.py

--- src/keepup_scrappers/items.py
+++ src/keepup_scrappers/items.py
@@ -46,3 +46,13 @@ class SnopesItem(scrapy.Item):
     label = scrapy.Field()
     content = scrapy.Field()
     author = scrapy.Field()
+
+class PolitifactItem(scrapy.Item):
+    title = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+    detail_url = scrapy.Field()
+    publication_date = scrapy.Field()
+    label = scrapy.Field()
+    content = scrapy.Field()
+    author = scrapy.Field()
