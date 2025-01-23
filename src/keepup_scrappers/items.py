10d4f961390db88e91c3bd585e182befe503eea7
SarahNasir54 <sarah.nasir540@gmail.com>
2025-01-23 10:25:03 +0500

    created items of snopes in items.py

--- src/keepup_scrappers/items.py
+++ src/keepup_scrappers/items.py
@@ -36,3 +36,13 @@ class FridayTItem(scrapy.Item):
     detail_url = scrapy.Field()
     publication_date = scrapy.Field()
     content = scrapy.Field()
+
+class SnopesItem(scrapy.Item):
+    title = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+    detail_url = scrapy.Field()
+    publication_date = scrapy.Field()
+    label = scrapy.Field()
+    content = scrapy.Field()
+    author = scrapy.Field()
