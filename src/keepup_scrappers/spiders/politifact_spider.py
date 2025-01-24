7e3302f58ce4c6115c9bbd0a7242a57129149c8d
SarahNasir54 <sarah.nasir540@gmail.com>
2025-01-24 11:19:39 +0500

    Corrected logic for politifact_spider.py

--- src/keepup_scrappers/spiders/politifact_spider.py
+++ src/keepup_scrappers/spiders/politifact_spider.py
@@ -8,23 +8,22 @@ class PolitifactSpider(BaseSpider):
     
     name = 'politifact_spider'
     page_counter = 1
+    site_key = 'politifact'
     
     custom_settings = {
-        "USER_AGENT" : 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
-        "ITEM_PIPELINES": {'scrapy.pipelines.images.ImagesPipeline': 1},
-        "IMAGES_STORE": 'data/politifact/images/',
-        "FEEDS": {
-            "data/politifact/data.json": {
-                "format": "json",
-                "encoding": "utf8",
-                "indent": 4,
-            },
-        }
+            "IMAGES_STORE": f'data/{site_key}/images/',
+            "FEEDS": {
+                f"data/{site_key}/data.json": {
+                    "format": "json",
+                    "encoding": "utf8",
+                    "indent": 4,
+                }
+            }
     }
 
     def __init__(self, *args, **kwargs):
         # Pass site_key to the base class
-        kwargs['site_key'] = 'politifact'
+        kwargs['site_key'] = self.site_key
         super().__init__(*args, **kwargs)
         self.page_counter = 1
 
@@ -33,15 +32,16 @@ class PolitifactSpider(BaseSpider):
 
             item = PolitifactItem()
 
-            item['title'] = post.css(self.selectors['post_title']).get().strip()
-            relative_url = post.css(self.selectors['post_link']).get()
-            item['detail_url'] = response.urljoin(relative_url)
-            item['label'] = response.css(self.selectors['label']).get().strip()
+            item['title'] = post.css(self.selectors['post_title']).get(default='').strip()
+            relative_url = post.css(self.selectors['post_link']).get(default='')
+            item['detail_url'] = response.urljoin(relative_url) if relative_url else [] 
+            item['label'] = response.css(self.selectors['label']).get(default='').strip()
 
             yield scrapy.Request(
                 url=item['detail_url'],
                 callback=self.parse_details,
                 meta={'item': item},
+                errback=self.handle_error,
             )
 
         self.logger.info(f"Page {self.page_counter} completed")
@@ -51,15 +51,20 @@ class PolitifactSpider(BaseSpider):
             yield scrapy.Request(
                 url=response.urljoin(next_page),
                 callback=self.parse,
+                errback=self.handle_error,
             )
 
         
     def parse_details(self, response):
         item = response.meta['item']
-        item['author'] = response.css(self.selectors['author']).get()
-        item['content'] = ' '.join(response.css(self.selectors['content']).getall()).strip()
-        item['publication_date'] = response.css(self.selectors['post_date']).get().strip()
-        image_url = response.css(self.selectors['post_image']).get()
-        item['image_urls'] = [response.urljoin(image_url)]
+        item['author'] = response.css(self.selectors['author']).get(default='').strip()
+        item['publication_date'] = response.css(self.selectors['post_date']).get(default='').strip()
+        content_paragraphs = response.css(self.selectors['content']).getall()
+        item['content'] = ' '.join([p.strip() for p in content_paragraphs if p.strip()])
+        image_url = response.css(self.selectors['post_image']).get(default='')
+        item['image_urls'] = [response.urljoin(image_url)] if image_url else [] 
 
         yield item
+
+    def handle_error(self, failure):
+        self.logger.error(f"Request Failed: {failure.request.url}")
