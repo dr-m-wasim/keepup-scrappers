579b3422d2d24d335ff820743d184da9aca9fdeb
SarahNasir54 <sarah.nasir540@gmail.com>
2025-01-24 11:11:48 +0500

    Corrected logic for GFC_spider.py

--- src/keepup_scrappers/spiders/GFC_spider.py
+++ src/keepup_scrappers/spiders/GFC_spider.py
@@ -8,23 +8,22 @@ class GFCSpider(BaseSpider):
     
     name = 'gfc_spider'
     page_counter = 1
+    site_key = 'geofactcheck'
     
     custom_settings = {
-        "USER_AGENT" : 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
-        "ITEM_PIPELINES": {'scrapy.pipelines.images.ImagesPipeline': 1},
-        "IMAGES_STORE": 'data/geofactcheck/images/',
-        "FEEDS": {
-            "data/geofactcheck/data.json": {
-                "format": "json",
-                "encoding": "utf8",
-                "indent": 4,
-            },
+            "IMAGES_STORE": f'data/{site_key}/images/',
+            "FEEDS": {
+                f"data/{site_key}/data.json": {
+                    "format": "json",
+                    "encoding": "utf8",
+                    "indent": 4,
+                }
+            }
         }
-    }
 
     def __init__(self, *args, **kwargs):
         # Pass site_key to the base class
-        kwargs['site_key'] = 'geofactcheck'
+        kwargs['site_key'] = self.site_key
         super().__init__(*args, **kwargs)
 
     def get_payload_headers(self, page_no):
@@ -51,16 +50,17 @@ class GFCSpider(BaseSpider):
 
             item = GFCItem()
 
-            item['title'] = post.css(self.selectors['post_title']).get()
-            image_url = post.css(self.selectors['post_image']).get()
-            item['image_urls'] = [response.urljoin(image_url)]   
-            item['detail_url'] = post.css(self.selectors['post_link']).get()
-            item['publication_date'] = post.css(self.selectors['post_date']).get()
+            item['title'] = post.css(self.selectors['post_title']).get(default='').strip()
+            image_url = post.css(self.selectors['post_image']).get(default='')
+            item['image_urls'] = [response.urljoin(image_url)] if image_url else []
+            item['detail_url'] = post.css(self.selectors['post_link']).get(default='').strip()
+            item['publication_date'] = post.css(self.selectors['post_date']).get(default='').strip()
 
             yield scrapy.Request(
                 url=item['detail_url'],
                 callback=self.parse_details,
                 meta={'item': item},
+                errback=self.handle_error,
             )
 
 
@@ -70,12 +70,17 @@ class GFCSpider(BaseSpider):
 
         yield scrapy.FormRequest(url = self.start_urls[0], 
                                  formdata = payload, 
-                                 callback = self.parse)
+                                 callback = self.parse,
+                                 errback=self.handle_error)
         
 
     def parse_details(self, response):
         item = response.meta['item']
-        item['author'] = response.xpath(self.selectors['author']).get()
-        item['content'] = ' '.join(response.css(self.selectors['content']).getall()).strip()
+        item['author'] = response.xpath(self.selectors['author']).get(default='')
+        content_paragraphs = response.css(self.selectors['content']).getall()
+        item['content'] = ' '.join([p.strip() for p in content_paragraphs if p.strip()])
 
         yield item
+
+    def handle_error(self, failure):
+        self.logger.error(f"Request Failed: {failure.request.url}")
