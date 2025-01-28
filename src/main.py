94e8b507e5458a55af66e6ee6a6c67c613b23ca4
sehrishsafdar <sehrishsafdar3@gmail.com>
2025-01-28 09:15:04 -0800

    corrected code of test_geofactcheck

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from scrapy.crawler import CrawlerProcess
 from keepup_scrappers.spiders.iverify_spider import IverifySpider
+from keepup_scrappers.spiders.GFC_spider import GFCSpider
 from scrapy.utils.project import get_project_settings
 
 def run_scrapers():
     settings = get_project_settings()
     process = CrawlerProcess(settings)
-    process.crawl(IverifySpider)
+    process.crawl(GFCSpider)
     process.start()
  
 if __name__ == '__main__':
