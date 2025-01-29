b1b09c0140dd528b0c0089ef4d1adffd7b4db369
sehrishsafdar <sehrishsafdar3@gmail.com>
2025-01-29 14:49:39 +0500

    corrected site key of pak.i-verify

--- src/keepup_scrappers/spiders/pak.i-verify_spider.py
+++ src/keepup_scrappers/spiders/pak.i-verify_spider.py
@@ -4,8 +4,8 @@ from keepup_scrappers.items import IVerifyItem
 
 class IverifySpider(BaseSpider):
     
-    name = 'iverify_spider'
-    site_key = 'iverify'
+    name = 'pak.i-verify_spider'
+    site_key = 'pak.i-verify'
 
     custom_settings = {
             "IMAGES_STORE": f'data/{site_key}/images/',
