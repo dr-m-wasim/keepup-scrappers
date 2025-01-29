89dc8fa29f442dbc3a93bd350cef92614c715fa4
SarahNasir54 <sarah.nasir540@gmail.com>
2025-01-29 11:49:52 +0500

    Added items for Business Recorder in items.py

--- src/keepup_scrappers/items.py
+++ src/keepup_scrappers/items.py
@@ -114,3 +114,15 @@ class HumEnglishItem(scrapy.Item):
     content = scrapy.Field()
     exerpt = scrapy.Field()
     label = scrapy.Field()
+
+class BRecorderItem(scrapy.Item):
+    title = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+    detail_url = scrapy.Field()
+    category = scrapy.Field()
+    exerpt = scrapy.Field()
+    publication_date = scrapy.Field()
+    content = scrapy.Field()
+    author = scrapy.Field()
+    
