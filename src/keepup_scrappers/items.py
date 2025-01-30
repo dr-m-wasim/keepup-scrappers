00ad1ffcca83d36a9553845be38af2cf7697f667
SarahNasir54 <sarah.nasir540@gmail.com>
2025-01-30 11:47:57 +0500

    Added Jang Items in items.py

--- src/keepup_scrappers/items.py
+++ src/keepup_scrappers/items.py
@@ -161,3 +161,15 @@ class HumEnglishItem(scrapy.Item):
     publication_date = scrapy.Field()
     content = scrapy.Field()
     author = scrapy.Field()
+
+class JangItem(scrapy.Item):
+    title = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+    detail_url = scrapy.Field()
+    exerpt = scrapy.Field()
+    publication_date = scrapy.Field()
+    content = scrapy.Field()
+    author = scrapy.Field()
+    content = scrapy.Field()
+    category = scrapy.Field()
