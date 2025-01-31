6b323045e5525a2d3c7cd39a0466f9dc0888a030
SarahNasir54 <sarah.nasir540@gmail.com>
2025-01-31 12:13:57 +0500

    Added samaatv_spider.py in spiders

--- src/keepup_scrappers/items.py
+++ src/keepup_scrappers/items.py
@@ -211,3 +211,13 @@ class PakistanTodayItem(scrapy.Item):
     publication_date = scrapy.Field()
     content = scrapy.Field()
     author = scrapy.Field()
+
+class SamaaTVItem(scrapy.Item):
+    title = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+    detail_url = scrapy.Field()
+    exerpt = scrapy.Field()
+    publication_date = scrapy.Field()
+    content = scrapy.Field()
+    author = scrapy.Field()
