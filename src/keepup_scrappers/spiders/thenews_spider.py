1af33a53b6807440beef1d3b12cf8d97f197d244
SarahNasir54 <sarah.nasir540@gmail.com>
2025-02-11 15:48:39 +0500

    Added thenews_spider.py working for all urls

--- src/keepup_scrappers/spiders/thenews_spider.py
+++ src/keepup_scrappers/spiders/thenews_spider.py
@@ -27,32 +27,35 @@ class TheNewsSpider(BaseSpider):
         self.page_counter = 1
 
     def parse(self, response):
+        categories = ["latest_stories", "business"]
+        selectors = next((self.selectors_map.get(cat) for cat in categories if cat in self.selectors_map), None)
             
-        for index, post in enumerate(response.css(self.selectors['single_post'])):
+        for index, post in enumerate(response.css(selectors['single_post'])):
 
             item = TheNewsItem()
             
-            item['title'] = post.css(self.selectors['post_title']).get(default='').strip()
-            item['detail_url'] = post.css(self.selectors['post_link']).get(default='').strip()
-            image_urls = response.css(self.selectors['post_image']).getall()  
+            item['title'] = post.css(selectors['post_title']).get(default='').strip()
+            item['detail_url'] = post.css(selectors['post_link']).get(default='').strip()
+            image_urls = response.css(selectors['post_image']).getall()  
             item['image_urls'] = [image_urls[index]] if image_urls and index < len(image_urls) else []  
-            item['exerpt'] = post.css(self.selectors['exerpt']).get(default='').strip()
+            item['exerpt'] = post.css(selectors['exerpt']).get(default='').strip()
             
             
             yield scrapy.Request(
                 url = item['detail_url'],
                 callback = self.parse_details,
-                meta = {'item': item},
+                meta = {'item': item, 'selectors': selectors},
                 errback=self.handle_error,
             )
 
     def parse_details(self, response):
         item = response.meta['item']
-        item['author'] = response.xpath(self.selectors['author']).get(default='').strip()
+        selectors = response.meta.get('selectors', {})
+        item['author'] = response.xpath(selectors['author']).get(default='').strip()
         if item['author'] == '':
-            item['author'] = response.css(self.selectors['author2']).get(default='').strip()
-        item['publication_date'] = response.css(self.selectors['post_date']).get(default='').strip()
-        content_paragraphs = response.css(self.selectors['content']).getall()
+            item['author'] = response.css(selectors['author2']).get(default='').strip()
+        item['publication_date'] = response.css(selectors['post_date']).get(default='').strip()
+        content_paragraphs = response.css(selectors['content']).getall()
         item['content'] = ' '.join([p.strip() for p in content_paragraphs if p.strip()])
 
         yield item
